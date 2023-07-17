a43984974d2addda73b0e7233548c8b8520d8622
Rinat <izmailov_rinat@list.ru>
2023-07-17 16:11:55 +0500

    17/07/16-00

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-const a = () => {
-console.log('privet')
+const myCity = {
+    city: 'New York',
+    popular: true,
+    country: 'USA'
 }
-a()
+
+myCity.country = 'Canada'
+
+myCity.city = 'Calgary'
+
+myCity.yearOfArrive = 2024
+
+myCity.monthOfArrive = 'ХЗ'
+
+myCity['monthOfArrive'] = 'ХЗ'
+
+
+
+console.log(myCity)

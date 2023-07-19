29e3b354ff6dec4f732b06bd73a71089492e97d4
Rinat <izmailov_rinat@list.ru>
2023-07-19 16:49:59 +0500

    zakonchil functions

---
+++ function.js
@@ -0,0 +1,66 @@
+//let a =3
+//let b =5
+//let c
+//c = a + b
+//console.log(c)
+//
+//a = 10
+//b = 12
+//c = a + b
+//console.log(c)
+
+//let a = 5
+//let b = 10
+//function sum(a, b) {
+//    const c = a + b
+//    console.log(c)
+//}
+
+//sum(a, b)
+//
+//a = 3
+//b = 4
+//
+//sum(a, b)
+//
+//a = 5
+//b = 12
+//
+// sum(a, b)
+
+//function tutTut(a, b) {
+//    let c
+//    a = a++
+//    c = a + b
+//    return c
+//}
+//
+//tutTut(25, 10)
+
+//const personq = {
+//    name: 'Bob',
+//    age: 25,
+//    secondName: 'Cop'
+//}
+//
+//const personw = {
+//    name: 'Bob',
+//    age: 25,
+//}
+//const persone = {
+//    age: 25,
+//    secondName: 'Cop'
+//}
+// function increasePersonAge(person) {
+//    person.age += 1
+//    return person
+// }
+//
+//increasePersonAge(personq)
+//console.log(personq.age)
+
+function printMyName() {
+    console.log('Rinat')
+}
+
+setTimeout( printMyName, 1500)

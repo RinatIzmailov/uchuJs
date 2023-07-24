1f57f8e52d0e6f1c7d3753318f82e60b9ae9dc6d
Rinat <izmailov_rinat@list.ru>
2023-07-24 14:59:54 +0500

    operatory

---
+++ 24.07.js
@@ -0,0 +1,30 @@
+//const a = 5
+//
+//function myFn() {
+//    function innerFn() {
+//        console.log(a)
+//    }
+//    innerFn()
+//}
+//
+//myFn()
+
+//Boolean('asd')
+//5 || 6
+//
+//5 && 4
+//const button = {
+    width:300,
+    text: 'ggg'
+//}
+//
+//const redButton = {
+//    ...button,
+//    color: 'red',
+//    background: 'blue'
+//}
+//console.table(redButton)
+const name = 'Rinat'
+const city = 'Brandon'
+ const rrr = `${'Меня'} ${'зовут'} ${name}${'.'} ${'Я'} ${'живу'} ${'в'} ${'городе'} ${city}${'.'}`
+  console.log(rrr)
